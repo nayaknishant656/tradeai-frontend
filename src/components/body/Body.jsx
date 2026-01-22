bedf258b6a767b42d24becec28ab3054ef79846c
nayaknishant656 <nayaknishant656@gmail.com>
2026-01-22 23:16:02 +0530

    Dashboard with JWT and Authentication

---
+++ src/components/body/Body.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Body = () => {
+    return (
+        <div className="border border-solid border-black">
+            <h1>Body</h1>
+        </div>
+    );
+};
+
+export default Body;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="border border-solid border-black">
+            <h1>Header</h1>
+        </div>
+    );
+};
+
+export default Header;
